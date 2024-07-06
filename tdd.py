c1f7e93f6b1ab464f5a2529ea4af98ee007db5a8
Kanza Jovita <kanzajovita@gmail.com>
2024-07-06 15:14:30 +0300

    passed with flying colors

--- tdd.py
+++ tdd.py
@@ -1,5 +1,30 @@
 def multiply(a, b):
     product = a * b
-    product == 1
-    return 1
-multiply(1,2)
+    # product == 1
+    return product
+multiply(1,1)
+
+def multiply(a, b):
+    product = a * b
+    return product
+multiply(2,2)
+
+
+def multiply(a, b):
+    product = a * b
+    # product == 9
+    return product
+multiply(3,3)
+
+def multiply(a, b):
+    product = a * b
+    # product == 16
+    return product
+multiply(4,4)
+
+
+def multiply(a, b):
+    product = a * b
+    # product == 1035
+    return product
+multiply(23,45)
--- test_tdd.py
+++ test_tdd.py
@@ -1,5 +1,21 @@
 from tdd import multiply
 
-def test_multiply_1_by_1():
-    product = multiply(1,1)
-    assert product == 1
+# def test_multiply_1_by_1():
+#     product = multiply(1,1)
+#     assert product == 1
+
+def test_multiply_2_by_2():
+    product = multiply(2,2)
+    assert product == 4
+
+def test_multiply_3_by_3():
+    product = multiply(3,3)
+    assert product == 9
+
+def test_multiply_4_by_4():
+    product = multiply(4,4)
+    assert product == 16
+
+def test_multiply_23_by_45():
+    product = multiply(23,45)
+    assert product == 1035
